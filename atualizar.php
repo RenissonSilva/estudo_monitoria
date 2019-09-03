2b2102fc4209b6cdb7e87a9b6ebca08f01618dc2
RenissonSilva <renissonx@gmail.com>
2019-09-02 23:18:07 -0300

    Correção de erros

--- atualizar.php
+++ atualizar.php
@@ -3,11 +3,4 @@
     $handle = fopen('dados.csv', 'a');
     fwrite($handle, $dado);
 ?>
-<pre>
-olá
-    mundo
-        cruel
-</pre>
-<h1>tá escrito</h1>
-<a href="tabela.php">Voltar</a>
 <?php header('location:tabela.php'); ?>
